c83d09428323a84cb1da2ae86780d30f0173f14f
adrianboh@gmail.com <adrianboh@gmail.com>
2023-04-12 09:47:01 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-post/Attributes/MoreThanZero.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_static.Attributes
+{
+    public class MoreThanZero : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object? value, ValidationContext _)
+        {
+            var input = Convert.ToDouble(value);
+
+            if (input <= 0)
+            {
+                return new ValidationResult(ErrorMessage ?? "Il campo deve esserer maggiore di zero");
+            }
+
+            return ValidationResult.Success!;
+        }
+    }
+}
